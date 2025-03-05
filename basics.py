5af1e8de47bd5b62e440bc7b6a656e656ead178d
jai <jaip7895@gmail.com>
2025-03-06 00:33:40 +0530

    variables

---
+++ basics.py
@@ -0,0 +1,26 @@
+import math
+print(math.sqrt(16))
+
+#This is a comment
+print("the above line is a comment")
+
+'''
+This is a Docstring
+
+it is used for defining  the function or a class 
+'''
+
+def add(a, b):
+    """
+    This function adds two numbers.
+            
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+            
+    Returns:
+    int: The sum of the two numbers.
+    """
+    return a + b
+
+print(add(66,3))
---
+++ variables.py
@@ -0,0 +1,39 @@
+age = 38
+name = "Ankit"
+weight = 58.5
+
+print(age)
+print(name)
+print(weight)
+
+#-------------------------------------------------------------------------------
+
+price = 100
+tax = 18
+total_price = price + tax
+
+print(total_price) 	# Output: 118
+
+#-------------------------------------------------------------------------------
+x = 10
+y = "geek"
+z = 20
+w = z
+
+print(x, y, z, w)	# Output: 10 geek 20 20
+
+
+#-------------------------------------------------------------------------------
+x = 10
+print(x)	# Output: 10
+
+x = "Python"
+print(x)	# Output: Python
+
+#-------------------------------------------------------------------------------
+is_valid = True
+is_published = False
+
+print(is_valid)			# Output: True
+print(is_published)		# Output: False
+
