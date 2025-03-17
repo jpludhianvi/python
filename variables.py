fd714fe828340e4de0e383f6d579fa43264f7ccb
jai <jaip7895@gmail.com>
2025-03-17 20:15:29 +0530

    variables complete

--- variables.py
+++ variables.py
@@ -37,3 +37,39 @@ is_published = False
 print(is_valid)			# Output: True
 print(is_published)		# Output: False
 
+#-------------------------------------------------------------------------------
+
+x = 100
+y = 200
+
+# Pythonic swapping
+x, y = y, x
+
+#-------------------------------------------------------------------------------
+
+x, y, z = 100, "Geeks", 10.5
+
+print(x)  # Output: 100
+print(y)  # Output: Geeks
+print(z)  # Output: 10.5
+
+#-------------------------------------------------------------------------------
+
+print(id(5))  # Unique identifier for the object with value 5
+a = 10
+print(id(a))  # Unique identifier for the object with value 10
+b = a
+print(id(b))  # Same identifier as a because both refer to the same object
+
+a = 10
+b = 10
+print(id(a))  # Identifier for the object with value 10
+print(id(b))  # Same identifier as a because a and b refer to the same object
+
+#-------------------------------------------------------------------------------
+
+g = [1, 2, 3]
+print(type(g)) # Output: <class 'list'>
+
+c = 2 + 4j
+print(type(c))	# Output: <class 'complex'>
